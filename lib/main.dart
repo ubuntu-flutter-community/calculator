0b8caeb8165aed1f5c06ccf9ffc00905eca0b94a
J-P Nurmi <jpnurmi@gmail.com>
2023-01-19 23:01:17 +0100

    Switch to YaruWindowTitleBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
 
 import 'calculator_page.dart';
 
 void main() {
   runApp(YaruTheme(
-    builder: (context, yaru, child) {
-      return MaterialApp(
-        theme: yaru.theme,
-        darkTheme: yaru.darkTheme,
-        routes: const {
-          Navigator.defaultRouteName: CalculatorPage.create,
-        },
-      );
-    },
+    builder: (context, yaru, child) => MaterialApp(
+      theme: yaru.theme,
+      darkTheme: yaru.darkTheme,
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) => Scaffold(
+        appBar: const YaruWindowTitleBar(
+          title: Text('Calculator'),
+        ),
+        body: child,
+      ),
+      routes: const {
+        Navigator.defaultRouteName: CalculatorPage.create,
+      },
+    ),
   ));
 }
