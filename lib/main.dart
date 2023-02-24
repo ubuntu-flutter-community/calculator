c35cae3a37f7c56bdcb693d9ad27d4e0a08159b6
J-P Nurmi <jpnurmi@gmail.com>
2023-02-24 19:26:02 +0100

    L10n (#9)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:yaru/yaru.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
@@ -10,9 +11,11 @@ void main() {
       theme: yaru.theme,
       darkTheme: yaru.darkTheme,
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       builder: (context, child) => Scaffold(
-        appBar: const YaruWindowTitleBar(
-          title: Text('Calculator'),
+        appBar: YaruWindowTitleBar(
+          title: Text(AppLocalizations.of(context).windowTitle),
         ),
         body: child,
       ),
